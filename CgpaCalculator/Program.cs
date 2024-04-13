93ff55422a4bb1310feb8839b27aaea15473b87b
Ibrahim Temitope Ajumoko <Ajumokoibrahimtemitope@gmail.com>
2024-04-13 16:11:36 +0100

    update

--- CgpaCalculator/Program.cs
+++ CgpaCalculator/Program.cs
@@ -16,6 +16,7 @@ namespace CgpaCalculator
 
             student.GetCourses();
             Console.WriteLine($"The student has registered {student.GetStudentRegisteredCourses()} courses");
+            Console.WriteLine($"Your CGPA is {student.CalcCgpa()}");
         }
     }
 
@@ -60,6 +61,62 @@ namespace CgpaCalculator
         {
             int registeredCourses = Courses.Count;
             return registeredCourses;
+        }
+
+        private decimal GetCourseScorePoint(int score, int unit)
+        {
+            decimal resultScale = 0;
+            if (score >= 75)
+            {
+                resultScale = 4;
+            }
+            else if (score >= 70 && score <= 74)
+            {
+                resultScale = 3.5M;
+            }
+            else if (score >= 65 && score <= 69)
+                resultScale = 3.25M;
+            else if (score >= 60 && score <= 64)
+            {
+                resultScale = 3;
+            }
+            else if (score >= 55 && score <= 59)
+                resultScale = 2.75M;
+            else if (score >= 50 && score <= 54)
+                resultScale = 2.5M;
+            else if (score >= 45 && score <= 49)
+            {
+                resultScale = 2.25M;
+            }
+            else if (score >= 40 && score <= 44)
+                resultScale = 2;
+            else
+            {
+                resultScale = 0;
+            }
+            return resultScale * unit;
+        }
+
+        private int SumTotalUnit()
+        {
+            int totalUnit = 0;
+            for (int i = 0; i < Courses.Count; i++)
+            {
+                totalUnit += Courses[i].CourseUnit;
+            }
+            return totalUnit;
+        }
+
+        public decimal CalcCgpa()
+        {
+            decimal result = 0;
+            for (int i = 0; i < Courses.Count; i++)
+            {
+                result += GetCourseScorePoint(Courses[i].Score, Courses[i].CourseUnit);
+            }
+
+            result /= SumTotalUnit();
+            return result;
         }
     }
 
